551539fc0ce6ae34a9397a3f163890be3d68c967
Oleksandr Topchii <o.topchii@plarium.com>
2018-11-20 17:11:53 +0200

    remove redundant

--- ConfigTool/Configuration/Startup.cs
+++ ConfigTool/Configuration/Startup.cs
@@ -32,12 +32,6 @@ namespace ConfigTool.Configuration
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
-            {
-                var context = serviceScope.ServiceProvider.GetService<ApplicationContext>();
-                context.Database.Migrate();
-            }
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
